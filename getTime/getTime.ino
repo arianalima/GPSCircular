0eaf6fa5eb00390c4ab9783403c5c09c43955ce8
Bernardo-PC\Bernardo <bernardodemsj@gmail.com>
2017-12-17 20:18:06 -0300

    classe para pegar hora atual

---
+++ getTime/getTime.ino
@@ -0,0 +1,35 @@
+#include <ESP8266WiFi.h>
+#include <time.h>
+
+const char* ssid = "GVT-BBB7";
+const char* password = "5067015358";
+
+int timezone = -3;
+int dst = 0;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("\nConnecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  configTime(timezone * 3600, 0, "pool.ntp.org", "time.nist.gov");
+  Serial.println("\nWaiting for time");
+  while (!time(nullptr)) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("");
+}
+
+void loop() {
+  time_t now = time(nullptr);
+  Serial.println(ctime(&now));
+  delay(10000);
+}
