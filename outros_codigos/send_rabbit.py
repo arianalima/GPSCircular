f421932576465349d40f5c7b469fdcd6f1196b0a
Ariana <ariana.lguimaraes@ufrpe.br>
2018-02-03 17:03:15 -0300

    Update send_rabbit.py

--- outros_codigos/send_rabbit.py
+++ outros_codigos/send_rabbit.py
@@ -4,19 +4,21 @@ import paho.mqtt.client as mqtt
 
 mqttc = mqtt.Client()
 mqttc.username_pw_set("psd", "psd")
-mqttc.connect("192.168.48.1", 1883)
+mqttc.connect("10.246.42.39", 1883)
 
 
 dicionario_segundos = {}
 
 with open("coleta1.txt") as arquivo:
     for linha in arquivo:
-        timestamp = int(linha.split(" ")[2])
-        mac = linha.split(" ")[0]
+        linha = linha.split(" ")
+        timestamp = int(linha[2])
+        rssi = int(linha[1])
+        mac = linha[0]
         if timestamp not in dicionario_segundos:
-            dicionario_segundos[timestamp] = [mac]
+            dicionario_segundos[timestamp] = [(mac,rssi)]
         elif mac not in dicionario_segundos[timestamp]:
-            dicionario_segundos[timestamp].append(mac)
+            dicionario_segundos[timestamp].append((mac,rssi))
 
 antes = int(time.time()) + 0
 
@@ -26,8 +28,8 @@ def worker(timestamp):
     if timestamp in dicionario_segundos.keys():
         lista = dicionario_segundos[timestamp]
         for i in lista:
-            mqttc.publish("hello", "{} {} {}p".format(i,rssi,timestamp))
-            print("mandei {} -99 {}p".format(i,timestamp))
+            mqttc.publish("hello", "{} {} {}p".format(i[0], i[1], timestamp))
+            print("mandei {} {} {}p".format(i[0], i[1], timestamp))
 
 while True:
     agora = time.time()
@@ -42,3 +44,10 @@ while True:
         print("------------------------------------------")
         print("------------------------------------------")
         break
+
+
+
+
+
+
+
