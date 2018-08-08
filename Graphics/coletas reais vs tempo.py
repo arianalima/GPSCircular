4d668f95bafb9d600b748f9a11cb654c75aecc9d
viiniciuspaes <viniciusjosepaes@gmail.com>
2018-08-08 20:25:27 -0300

    upload Coletas

--- Graphics/coletas reais vs tempo.py
+++ Graphics/coletas reais vs tempo.py
@@ -4,41 +4,43 @@ from functools import reduce
 import matplotlib.pyplot as plt
 import pandas as pd
 
-numero_coletas = 8
+numero_coletas = 28
 data_frames = []
 
 
 def create_df(lista, coleta):
-    arq = open("../Coletas/coleta {} real.txt".format(coleta), "r")
-    tempo = arq.readlines()
-    dado = []
-    data = []
+    try:
+        arq = open("../Coletas/coleta {} real.txt".format(coleta), "r")
+        tempo = arq.readlines()
+        dado = []
+        data = []
 
-    def get_dados(linha):
-        linha = linha.split()
-        data.append(linha[0])
-        dado.append(int(linha[1]))
-    list(map(lambda x: get_dados(x), tempo))
+        def get_dados(linha):
+            linha = linha.split()
+            data.append(linha[0])
+            dado.append(int(linha[1]))
+        list(map(lambda x: get_dados(x), tempo))
 
-    def reformat(indice):
-        data[indice] = float(data[indice].replace(":", "."))
+        def reformat(indice):
+            data[indice] = float(data[indice].replace(":", "."))
 
-    list(map(lambda x: reformat(x), range(len(data))))
+        list(map(lambda x: reformat(x), range(len(data))))
 
-    dic = {"coleta {}".format(coleta): dado, "hora": data}
-    df = pd.DataFrame(dic)
-    df = df.set_index("hora")
-    data_frames.append(df)
-    arq.close()
-
+        dic = {"coleta {}".format(coleta): dado, "hora": data}
+        df = pd.DataFrame(dic)
+        df = df.set_index("hora")
+        data_frames.append(df)
+        arq.close()
+    except:
+        pass
 
 list(map(lambda x : create_df(data_frames,x), range(1,numero_coletas)))
 # print(data_frames)
 result = reduce(lambda left, right: left.merge(right, how='outer', left_index=True, right_on="hora"),
                 data_frames)
 result = pd.DataFrame(result)
-plt.plot(result)
-plt.legend([*result.keys()])
+plt.plot(result, 'ro')
+# plt.legend([*result.keys()])
 plt.xlabel("Hora")
 plt.ylabel("Lotação")
 plt.show()
